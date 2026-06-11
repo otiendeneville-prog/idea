ad65fbe00a911703b95ed77b4f890981dc7d90d8
Neville <otiendeneville@gamil.com>
2026-06-11 05:10:26 +0300

    addition of models

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\idea;
+use App\Http\Requests\StoreideaRequest;
+use App\Http\Requests\UpdateideaRequest;
+
+class IdeaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreideaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateideaRequest $request, idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(idea $idea)
+    {
+        //
+    }
+}
---
+++ database/factories/IdeaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\idea;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<idea>
+ */
+class IdeaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
