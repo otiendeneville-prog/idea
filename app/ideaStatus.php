bc57d5fb1822f85829c9599783d34658c64aed66
Neville <otiendeneville@gamil.com>
2026-06-11 14:38:40 +0300

    model_factory add complete

--- app/ideaStatus.php
+++ app/ideaStatus.php
@@ -5,5 +5,6 @@ namespace App;
 enum ideaStatus: string
 {
     case PENDING = "pending";
-    case IN_PROGRESS ="in_progress"; 
+    case IN_PROGRESS = "in_progress";
+    case COMPLETE = "complete";
 }
