ab9950f881c718bdd000eda211438da8e357e15e
Neville <otiendeneville@gamil.com>
2026-06-11 11:48:53 +0300

    change the spelling of the unguard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::ungurd();
+        Model::unguard();
         Model::shouldBeStrict();
         User::where(...);
         Model::AutomaticallyEgerLoadRelationship();
