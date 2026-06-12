f025ea59b2f5bbdb9a7c880c17f75b61c120e8aa
Neville <otiendeneville@gamil.com>
2026-06-12 08:44:18 +0300

    add user belongsto model

--- app/Models/idea.php
+++ app/Models/idea.php
@@ -6,6 +6,7 @@ use App\ideaStatus;
 use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class idea extends Model
 {
@@ -15,4 +16,8 @@ class idea extends Model
        'links'=>AsArrayObject::class,
        'status'=>ideaStatus::class
     ];
+    public function user(): BelongsTo
+    {
+      return $this->belongsTo(User::class);
+    }
 }
--- app/ideaStatus.php
+++ app/ideaStatus.php
@@ -7,4 +7,14 @@ enum ideaStatus: string
     case PENDING = "pending";
     case IN_PROGRESS = "in_progress";
     case COMPLETE = "complete";
+
+public function labeL(): string
+{
+    return match($this){
+        self::PENDING =>'Pending',
+        self::IN_PROGRESS =>'In Progress',
+        self::COMPLETE =>'Completed',
+    };
+
+}
 }
