4052303eba483fd09b47a9ff1124bbcfec6e3c07
Neville <otiendeneville@gamil.com>
2026-06-12 07:50:55 +0300

    tinker idea

--- app/Models/idea.php
+++ app/Models/idea.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\ideaStatus;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,7 +12,7 @@ class idea extends Model
     /** @use HasFactory<\Database\Factories\IdeaFactory> */
     use HasFactory;
     protected $casts = [
-       'links'=>AsArrayObjecct::class,
-       'status'=>IdeaStatus::class
-    ]
+       'links'=>AsArrayObject::class,
+       'status'=>ideaStatus::class
+    ];
 }
