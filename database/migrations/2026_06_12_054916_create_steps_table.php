29c21df66ad9182950f7c6f33764997e51699b06
Neville <otiendeneville@gamil.com>
2026-06-12 09:06:56 +0300

    add boolean and string table

--- database/migrations/2026_06_12_054916_create_steps_table.php
+++ database/migrations/2026_06_12_054916_create_steps_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
     {
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            
+            $table->string('description');
+            $table->boolean('completed');
+
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    //   Reverse the migrations.
+     
     public function down(): void
     {
         Schema::dropIfExists('steps');
